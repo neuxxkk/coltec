b6829afcdd3d04aeccfb39751fe6473ed5a04ad8
neuxxkk <83613773+neuxxkk@users.noreply.github.com>
2023-02-23 14:59:19 -0300

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int void(){
+	int type,x,y;
+	printf("Welcome to the Calculator in C!\n\nChose a operation:\n");
+	printf("1 - Sum\n2 - Subtraction\n3 - Multiplication\n4 - Division");
+
+	scanf("%d", &type);
+	if (type == 1){
+		printf("Type the first number:");
+		scanf("%f", &x);
+		printf("Type the second number:")
+
+	}
+	return 0;
+}

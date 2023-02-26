0802886a3ff484a77d03f5b2054d5f97027f8b1b
neuxxkk <vitornms@gmail.com>
2023-02-26 03:22:00 -0300

    a

--- scratch/calculator.c
+++ scratch/calculator.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-int void(){
-	int type,x,y;
-	printf("Welcome to the Calculator in C!\n\nChose a operation:\n");
-	printf("1 - Sum\n2 - Subtraction\n3 - Multiplication\n4 - Division");
 
-	scanf("%d", &type);
-	if (type == 1){
-		printf("Type the first number:");
-		scanf("%f", &x);
-		printf("Type the second number:")
+void input(char *msg, float *ans){
+		printf("%s",msg);
+		scanf("%f", ans);
+}
 
+int main(){
+		float type, x, y, res;
+		printf("Welcome to the Calculator in C!\n\nChose a operation:\n");
+		printf("1 - Sum\n2 - Subtraction\n3 - Multiplication\n4 - Division\n:");
+
+		scanf("%f", &type);
+		if (type == 1){
+				input("Type the first number: ", &x);
+				input("Type the second number: ", &y);
+			
+				res = x+y;
+
+				printf("The result is: %.2f\n",res);
 	}
-	return 0;
+		return 0;
 }
--- scratch/everythinhg.c
+++ scratch/everythinhg.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void input(char *message, char *value) {
+    printf("%s", message);
+    scanf("%s", value);
+}
 
 int main() {
     char name[100];
     int i, min, max, age;
 
-    printf("Whats your name?\n");
-    scanf("%s", name);
+    input("Hello what's your name? ", name);
 
     printf("Hello %s!\n",name);
     sleep(1);
 
-    printf("This is an Adult program (+18)\nHow olds are you?");
+    printf("This is an Adult program (+18)\nHow old are you? ");
     scanf("%d", &age);
 
-    if (age<18){
-        printf("NO! You can't acces this site!\n");
+    if (age < 18) {
+        printf("NO! You can't access this site!\n");
         return 0;
     }
-    else{
-        printf("OK! You are allowed!");
-        }
+    else {
+        printf("OK! You are allowed!\n");
+    }
 
     printf("Type a range\nMin: ");
     scanf("%d", &min);
     printf("Max: ");
     scanf("%d", &max);
 
-    for (i=min;i<=max;i++){
-    printf("%d\n", i);
-    usleep(500000);
+    for (i = min; i <= max; i++) {
+        printf("%d\n", i);
+        usleep(500000);
     }
 
     return 0;
 }
+

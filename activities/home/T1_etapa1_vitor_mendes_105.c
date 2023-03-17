4b55c5eaa5d456f513025e560fee3b15e26259f8
neuxxkk <vitornms@gmail.com>
2023-03-16 23:51:47 -0300

    a

--- activities/home/T1_etapa1_vitor_mendes_105.c
+++ activities/home/T1_etapa1_vitor_mendes_105.c
@@ -1,68 +1,117 @@
-
-    /*q = '\033['
-
-    #style
-    default = f'{q}0m'
-    bold = f'{q}1m'
-    bright = f'{q}2m'
-    italic = f'{q}3m'
-    underl = f'{q}4m'
-
-    #color
-    black_font = f'{q}30m'
-    red_font = f'{q}31m'
-    green_font = f'{q}32m'
-    yellow_font = f'{q}33m'
-    blue_font = f'{q}34m'
-    purple_font = f'{q}35m'
-    light_blue_font = f'{q}36m'
-    white_font = f'{q}37m'
-
-    #bg_color
-    black_bg = f'{q}40m'
-    red_bg = f'{q}41m'
-    green_bg = f'{q}42m'
-    yellow_bg = f'{q}43m'
-    blue_bg = f'{q}44m'
-    purple_bg = f'{q}45m'
-    light_blue_bg = f'{q}46m'
-    white_bg = f'{q}47m'*/
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#ifdef _WIN32
+    char clean[100] = "cls";
+#elif __linux__ || __APPLE__
+    char clean[100] = "clear";
+#else 
+    #error "Not supported operational system."
+#endif
 
- char *titulos[] = {"Calculo dos juros reais",
-                       "Conversao de moedas",
-                       "Calculo da reserva de emergência",
-                       "Juros compostos com deposito mensal",
-                       "Poupanca para a faculdade",
-                       "Plano para juntar dinheiro",
-                       "Simulador de empréstimo",
-                       "Calculo do imposto de renda",
-                       "Independência Financeira"};
+int option;
 
-void init(){
-		for(int i=0; i<=52; i++){
-			if(i==26){printf("\n\t\033[1;31mMYMONEY – DIGITE A OPÇÃO DESEJADA\n\033[0m");}
-			else{printf("-=");}
-	}
+char *titulos[] = {
+    "Calculo dos juros reais",
+    "Conversão de moedas",
+    "Calculo da reserva de emergência",
+    "Juros compostos com deposito mensal",
+    "Poupança para a faculdade",
+    "Plano para juntar dinheiro",
+    "Simulador de empréstimo",
+    "Cálculo do imposto de renda",
+    "Independência Financeira"
+};
 
-	printf("\n");
+void input(char *type, char *msg, void *var){
+    if(msg == NULL){msg = "";}
+    printf("%s", msg);
+    scanf(type, var);
+}
 
-	for (int n = 1; n < 10; ++n)
-	{
-		printf("%i. %s\n", n, titulos[n-1]);
-	}
+void menu(){
+    system(clean);
+    for(int i=0; i<=100; ++i){
+        if(i==50){printf("\nMYMONEY – DIGITE A OPÇÃO DESEJADA\n");}
+        else{printf("*");}
+    }
+    
+    printf("\n");
 
-	printf("\n");
+    for (int n = 1; n < 10; ++n){
+        printf("(%i) %s\n"    , n, titulos[n-1]);
+    }
 
-	for (int o = 0; o < 26; ++o)
-	{
-		printf("-=");
-	}
+    printf("(0) Sair\n");
 
-	printf("\n");
+    for (int o = 0; o < 50; ++o){
+        printf("*");
+    }
+
+    printf("\n: ");
+    input("%i", NULL, &option);
+}
+
+void back(){
+    int back;
+    input("%i", ("\n1. Menu \t\t\t\t 2. Sair\n: "), &back);
+    switch(back){
+        case 1:break;
+        case 2:exit(0);
+    }
+}
+
+void title(){
+    system(clean);
+    char str[100];
+    strcpy(str, titulos[option-1]);
+    for (int z = 0; str[z]; ++z){
+        str[z] = toupper(str[z]);
+    }
+
+    printf("\n%s\n", str);
+}
+
+//sec1
+void real_fee(){
+    title();
+    float nominal_fee, inflation, real_fee;
+    input("%f", "Digite a taxa de juros nominal a.a. em %: ", &nominal_fee);
+    input("%f", "Digite a taxa de inflação a.a. em %: ", &inflation);
+
+    real_fee = ((1+nominal_fee/100) / (1+inflation/100) - 1)*100;
+
+    printf("Taxa de juros reais a.a. em %: %.2f\n", real_fee);
+}
+
+//sec2
+void coin_conveter(){
+
+    title();
 }
 
 int main(){
-	init();
+    do{
+        menu();
+        switch(option){
+
+            case 0:break;
+
+            case 1:
+                real_fee();
+                back();
+                break;
+
+            case 2:
+                coin_conveter();
+                back();
+                break;
+
+            default:
+                printf("Opcão inválida\n");
+                sleep(1);
+        }
+    }while(option!=0);
+    return 0;
 }

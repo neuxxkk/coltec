5a29be2e62287aec412bef220b3411b343788657
neuxxkk <vitornms@gmail.com>
2024-03-28 17:19:10 -0300

    a

--- montanha/ex001/parte_3/Repeticao3.java
+++
@@ -1,8 +0,0 @@
-public class Repeticao3 {
-
-    public static void main(String[] args) {
-        for (int i=1; i<100; i++) {
-            System.out.print(i%7 == 0 ? i+"\n" : "");
-        }
-    }
-}

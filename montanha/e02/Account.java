0d12f85de2ca1405a165a76b6776d879a926e5f7
neuxxkk <vitornms@gmail.com>
2024-04-02 03:30:56 -0300

    a

--- montanha/e02/Account.java
+++ montanha/e02/Account.java
@@ -1,22 +1,3 @@
-class Client {
-    String name;
-    String cpf;
-    String adress;
-    int age;
-    String gender;
-
-    public Client(String name, String cpf, String adress, int age, char gender){
-        this.name = name;
-        this.cpf = cpf;
-        this.adress = adress;
-        this.age = age;
-        this.gender = (gender=='f') ? "Female": "Male";
-    }
-
-    void printClient(){
-        System.out.printf("\n--Account data--\nName: %s\nCPF: %s\nAdress: %s\nAge: %d\nGender: %s\n", this.name, this.cpf, this.adress, this.age, this.gender);
-    }
-}
 
 public class Account {
     Client owner;

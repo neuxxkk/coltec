052a9424f301edf769b15789baf0a25ea2b7c066
neuxxkk <vitornms@gmail.com>
2025-03-12 17:07:11 -0300

    a

---
+++ lucasAlves/ano3/ExercicioLINQ/ExercicioLINQ/Program.cs
@@ -0,0 +1,7 @@
+﻿using ConsoleDump;
+
+var cotas = CotaParlamentar.LerCotasParlamentares("cota_parlamentar.csv");
+cotas.OrderByDescending(c => c.ValorLiquido).Take(10).ToArray().Dump();
+
+cotas.GroupBy(c => c.DataEmissao?.Month)
+.Select(g => new { Mes = g.Key, Media = g.Average(v => v.ValorLiquido) }).Dump();
